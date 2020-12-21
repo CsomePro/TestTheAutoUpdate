0aee98464655e1e581df65da4610db6d6f15dd7b
csomePro <csome@m.scnu.edu.com>
2020-12-21 23:46:25 +0800

    test

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 import time
 import requests
 import os
+import zipfile
 
 api = 'https://api.github.com/repos/CsomePro/TestTheAutoUpdate'
-downloadUrl = 'https://github.com/CsomePro/TestTheAutoUpdate'
+downloadUrl = 'https://github.com/CsomePro/TestTheAutoUpdate/archive/master.zip'
 allInfo = requests.get(api).json()
 
-print(allInfo['updated_at'])
-print(time.mktime(time.strptime(allInfo['updated_at'], "%Y-%m-%dT%H:%M:%SZ")))
-print(os.path.getmtime('main.py'))
-name = 'main.py'
-print(requests.get(downloadUrl % name))
+# print(allInfo['updated_at'])
+# print(time.mktime(time.strptime(allInfo['updated_at'], "%Y-%m-%dT%H:%M:%SZ")))
+# print(os.path.getmtime('main.py'))
+#
+# 下载zip文件
+with open('tmp.zip', 'wb') as f:
+    f.write(requests.get(downloadUrl).content)
+# 解压zip文件
+zipFile = zipfile.ZipFile('tmp.zip')
+zipList = zipFile.namelist()
+# print(zipList)
+# abspath = os.path.dirname(os.path.abspath(__file__))
+# print(os.path.dirname(os.path.abspath(__file__)))
+zipFile.extract('TestTheAutoUpdate-master/main.py')
+zipFile.close()
+
+# 复制文件
+with open('TestTheAutoUpdate-master/main.py', 'r') as source:
+    with open('main.py', 'w') as destination:
+        destination.write(source.read())
+
+# 删除文件
+os.remove('tmp.zip')
+os.remove('TestTheAutoUpdate-master/main.py')
+os.rmdir('TestTheAutoUpdate-master')
+
+print("aaaa")
